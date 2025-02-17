a069bf1b5c56d83aa49e2d28f8ca9cd6c2692c3c
mawaeg <62243375+mawaeg@users.noreply.github.com>
2025-02-17 22:06:23 +0100

    [firmware] Fix syntax error

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P4, KC_P5, KC_P6, KC_NUM,
         KC_P1, KC_P2, KC_P3, KC_P0
     )
-}
+};
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
