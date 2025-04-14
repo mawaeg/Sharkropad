9ec838658c2f985d2cb8fb810ef64a3a6ff9047a
mawaeg <62243375+mawaeg@users.noreply.github.com>
2025-04-14 20:21:15 +0200

    [firmware] Various improvements
    Use PWM driver instead of bitbanging for rgb lighting
    Move oled task to sharkropad file and improve effect handling with automatic effect list generation and automatic scrolling for effect names that don't fit a single line

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -13,9 +13,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ 1 │ 2 │ 3 │ 0          │
      * └───┴───┴───┴────────────┘
      */
-        [0] = LAYOUT_numpad_4x4(
-                             RGB_MOD,
-        KC_P7, KC_P8, KC_P9, RGB_TOG,
+        [0] = LAYOUT(
+                             RM_NEXT,
+        KC_P7, KC_P8, KC_P9, RM_TOGG,
         KC_P4, KC_P5, KC_P6, KC_NUM,
         KC_P1, KC_P2, KC_P3, KC_P0
     )
@@ -24,77 +24,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(RGB_SPD, RGB_SPI)},
+    [0] = { ENCODER_CCW_CW(RM_SPDD, RM_SPDU)},
 };
-#endif
-
-#ifdef OLED_ENABLE
-
-const char *rgb_matrix_effect_names[] = {
-    "None",                             
-    "Solid Color",
-    "Alphas Mods",
-    "Gradient Up/Down",
-    "Gradient Left/Right",
-    "Breathing",
-    "Band Saturation",
-    "Band Value",
-    "Band Pinwheel Sat",
-    "Band Pinwheel Val",
-    "Band Spiral Sat",
-    "Band Spiral Val",
-    "Cycle All",
-    "Cycle Left/Right",
-    "Cycle Up/Down",
-    "Cycle Out/In",
-    "Cycle Out/In Dual",
-    "Rainbow Chevron",
-    "Cycle Pinwheel",
-    "Cycle Spiral",
-    "Dual Beacon",
-    "Rainbow Beacon",
-    "Rainbow Pinwheels",
-    "Flower Blooming",
-    "Raindrops",
-    "Jellybean Raindrops",
-    "Hue Breathing",
-    "Hue Pendulum",
-    "Hue Wave",
-    "Pixel Fractal",
-    "Pixel Flow",
-    "Pixel Rain",
-    "Typing Heatmap",
-    "Digital Rain",
-    "Reactive Simple",
-    "Reactive",
-    "Reactive Wide",
-    "Reactive Multiwide",
-    "Reactive Cross",
-    "Reactive Multicross",
-    "Reactive Nexus",
-    "Reactive Multinexus",
-    "Splash",
-    "Multisplash",
-    "Solid Splash",
-    "Solid Multisplash",
-    "Starlight",
-    "Starlight Dual Hue",
-    "Starlight Dual Sat",
-    "Riverflow",
-    "Effect Max"
-};
-
-bool oled_task_user(void) {
-
-    oled_write_ln_P(PSTR("Mode: "), false);
-    oled_write_ln(rgb_matrix_effect_names[rgb_matrix_get_mode()], false);
-
-    oled_write_ln_P(PSTR("RGB Speed: "), false);
-    char speedBuffer[4];
-    uint8_t speedPercentage = (uint8_t)(((float)rgb_matrix_get_speed() / 255.0) * 100.0);  // Proper float division
-    sprintf(speedBuffer, "%d", speedPercentage);
-    oled_write(speedBuffer, false);
-    oled_write("%  ", false);
-    return false;
-}
 #endif
